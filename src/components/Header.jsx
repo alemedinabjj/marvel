8231bfc2bf378ef4ff3671304dc2511505626c00
Alexandre de Souza Medina <alemedina@MacBook-de-Alexandre.local>
2022-07-15 15:31:45 -0300

    bug fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export function Header () {
   const [Header, setHeader] = useState(false);
 
   function scrollHeader() {
-    if (window.scrollY > 100) {
+    if (window.scrollY > 50) {
       setHeader(true);
     } else {
       setHeader(false);
@@ -29,13 +29,13 @@ export function Header () {
       {
 
         Header === true ? (
-          <header className="w-full p-0 transition fixed z-10 bg-red-700 flex flex-col gap-4">
+          <header className="w-full shadow-xl p-0 transition-all fixed z-10 bg-red-700 flex flex-col gap-4">
           <SearchBar />
         </header>
 
         ) :
         (
-          <header className="w-full p-5 transition bg-red-700 flex flex-col gap-4">
+          <header className="w-full shadow-2xl p-5 z-10 fixed transition-all bg-red-700 flex flex-col gap-4">
           <Navbar />
           <SearchBar />
         </header>
