c75d028d3b5d254d877dd4b502ba36def8c94cc2
Alexandre de Souza Medina <alemedina@MacBook-de-Alexandre.local>
2022-07-15 15:24:17 -0300

    scroll no header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,50 @@
 import { Navbar } from "./Navbar";
 import { SearchBar } from "./SearchBar";
+import { useState, useEffect } from "react";
+
 
 export function Header () {
+
+  const [Header, setHeader] = useState(false);
+
+  function scrollHeader() {
+    if (window.scrollY > 100) {
+      setHeader(true);
+    } else {
+      setHeader(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      scrollHeader();
+    })
+  },[]) 
+
+
   return (
-    <header className="w-full p-5 bg-red-700 flex flex-col gap-4">
-      <Navbar />
-      <SearchBar />
-    </header>
+      <>
+      
+      
+      {
+
+        Header === true ? (
+          <header className="w-full p-0 transition fixed z-10 bg-red-700 flex flex-col gap-4">
+          <SearchBar />
+        </header>
+
+        ) :
+        (
+          <header className="w-full p-5 transition bg-red-700 flex flex-col gap-4">
+          <Navbar />
+          <SearchBar />
+        </header>
+        )
+
+      }
+      
+      
+      </>
+
   )
 }
